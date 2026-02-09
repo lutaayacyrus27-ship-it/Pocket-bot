cc0cbbdef5f9f12c31fe9fcd35c05090b7901e80
lutaayacyrus27-ship-it <lutaayacyrus27@gmail.com>
2026-02-09 19:58:16 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+if not TELEGRAM_BOT_TOKEN or not TELEGRAM_CHAT_ID:
+    raise ValueError("Telegram variables not set")
